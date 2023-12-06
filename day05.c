99b9c1e1ffecf5613cfe873d6b67a9a12b9ba417
slemsvamp <silverljus@gmail.com>
2023-12-06 23:38:13 +0100

    Working on day05.

--- day05.c
+++ day05.c
@@ -7,6 +7,14 @@ typedef struct range
     u64 end;
 } range;
 
+typedef struct linked_range
+{
+    u64 start;
+    u64 end;
+    b32 transformed;
+    struct linked_range *next;
+} linked_range;
+
 typedef struct transformation
 {
     u64 source;
@@ -27,6 +35,16 @@ typedef enum seed_reading_mode
     SEED_READING_MODE_ranges,
 } seed_reading_mode;
 
+range
+transform_range(u64 start, u64 end, transformation transform)
+{
+    return (range)
+    {
+        .start = start + transform.source - transform.target,
+        .end = end + transform.source - transform.target
+    };
+}
+
 u64
 transform_seeds(file_data file, seed_reading_mode seedReadingMode)
 {
@@ -43,7 +61,7 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
     u64 seedCount = 0;
     range *seeds = calloc(4096, sizeof(range));
 
-    lexer_tokenizer t = { .at = file.data };
+    lexer_tokenizer t = lexer_tokenizer_create(file.data);
     lexer_token token = lexer_get_next(&t, LEXER_TOKEN_TYPE_number);
 
     // how to read
@@ -122,6 +140,9 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
     u64 nextRangeSeedCount = 0;
     range *nextRangeSeeds = calloc(RANGE_MAX, sizeof(range));
 
+    u64 linkedRangeCount = 0;
+    linked_range *linkedRanges = calloc(RANGE_MAX, sizeof(range));
+
     for (u64 seedIndex = 0; seedIndex < seedCount; seedIndex++)
     {
         rangeSeeds[rangeSeedCount++] = seeds[seedIndex];
@@ -138,14 +159,20 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
 
                 printf("  SEED %zu - %zu:\r\n", currentRangeSeed.start, currentRangeSeed.end);
 
-                b32 foundTransform = false;
+                linkedRanges[linkedRangeCount++] = (linked_range)
+                {
+                    .start = currentRangeSeed.start,
+                    .end = currentRangeSeed.end,
+                    .transformed = false,
+                    .next = 0
+                };
 
                 for (u64 transformationIndex = 0; transformationIndex < collection.count; transformationIndex++)
                 {
                     transformation transform = collection.items[transformationIndex];
-                    printf("    TRANSFORM %zu:\r\n", transformationIndex + 1);
-
                     u64 transformEnd = transform.source + transform.range - 1;
+                    
+                    printf("    TRANSFORM %zu (%zu to %zu): ", transformationIndex + 1, transform.source, transformEnd);
 
 #if 0
                     if (currentRangeSeed.start < transform.source)
@@ -169,23 +196,126 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
                         printf("!! SEED START: %zu, END: %zu, TRANSFORM SOURCE: %zu, RANGE: %zu, TARGET: %zu!\r\n",
                             currentRangeSeed.start, currentRangeSeed.end, transform.source, transform.range, transform.target);
                     }
+
                     //            [               ]
                     //    {             }
-                    //            |     |
+                    //    |   1   |  2  |
+
 // ------------------------------------------------------------------------
+
                     //            [               ]
-                    //    {                          }
-                    //            |               |
+                    //    {                           }
+                    //    |   1   |       2       | 3 |
+
 // ------------------------------------------------------------------------
+
                     //            [               ]
                     //                {        }
-                    //                |        |
+                    //                |    1   |
+
 // ------------------------------------------------------------------------
                     //            [               ]
                     //                {                 }
-                    //                |           |
+                    //                |     1     |  2  |
 #endif
 
+                    // +------- 1 -------+           +--2--+    +----------------- 3 ---------------+       
+                    // |                 |           |     |    |                                   |       
+                    //
+                    //             *******ooooooooooo*******oooo*************************************ooo    
+                    //
+                    //             |     |           |     |    |                                   |       
+                    //             +- 1 -+           +- 2 -+    +----------------- 3 ---------------+       
+
+                    // TRANSFORM PARTS of SEED RANGE, they get TRANSFORMED
+
+                    // UNTRANSFORMED PARTS after TRANSFORMS ARE DONE, get passed on
+
+                    // 1. fully outside, full UNTRANSFORMED (1 part)
+                    // 2. lefty (seed is left hanging) -> TRANSFORM transform.start to seed.end, seed.start to transform.start - 1 UNTRANSFORMED (2 parts)
+                    // 3. inside (seed is inside fully) -> TRANSFORM seed.start to seed.end (1 part)
+                    // 4. righty (seed is right hanging) -> TRANSFORM seed.start to transform.end, transform.end+1 to seed.end UNTRANSFORMED (2 parts)
+                    // 5. encompassing (seed covers entire transform) -> TRANSFORM transform area, LEFT AND RIGHT UNTRANSFORMED (3 parts)
+
+                    // the problem is that we actually have to care about what is being claimed
+                    // we go through all the transforms for this collection and for all untransformed values we need to continue them
+                    // we don't want to use arrays and such, so everything needs to be ranges
+
+                    // [%%%%][..][%%%%%%%%%%%%][%%%][.......][%%%%%%%%%][.....][%]
+
+                    // linked list? yuck?
+
+                    // % = TRANSFORMED
+                    // . = UNTRANSFORMED
+
+                    if (currentRangeSeed.end < transform.source || currentRangeSeed.start > transformEnd)
+                    {
+                        // FULLY OUTSIDE
+                        // FULL UNTRANSFORMED (1 part)
+                        printf("      FULL UNTRANSFORMED\r\n");
+                    }
+                    else if (currentRangeSeed.start < transform.source && currentRangeSeed.end >= transform.source && currentRangeSeed.end <= transformEnd)
+                    {
+                        // LEFTY
+                        // TRANSFORM transform.start to seed.end, seed.start to transform.start - 1 UNTRANSFORMED (2 parts)
+                        linked_range *lr = linkedRanges;
+
+                        while (lr != 0)
+                        {
+
+                            lr = lr->next;
+                        }
+                        
+                        nextRangeSeeds[nextRangeSeedCount++] = transform_range(transform.source, currentRangeSeed.end, transform);
+
+                        nextRangeSeeds[nextRangeSeedCount++] = (range)
+                        {
+                            .start = currentRangeSeed.start,
+                            .end = transform.source - 1
+                        };
+                        printf("      LEFTY\r\n");
+                    }
+                    else if (currentRangeSeed.start >= transform.source && currentRangeSeed.end <= transformEnd)
+                    {
+                        // INSIDE
+                        // TRANSFORM seed.start to seed.end (1 part)
+                        nextRangeSeeds[nextRangeSeedCount++] = transform_range(currentRangeSeed.start, currentRangeSeed.end, transform);
+                        printf("      INSIDE\r\n");
+                    }
+                    else if (currentRangeSeed.start >= transform.source && currentRangeSeed.start <= transformEnd && currentRangeSeed.end > transformEnd)
+                    {
+                        // RIGHTY
+                        // TRANSFORM seed.start to transform.end, transform.end+1 to seed.end UNTRANSFORMED (2 parts)
+                        nextRangeSeeds[nextRangeSeedCount++] = transform_range(currentRangeSeed.start, transformEnd, transform);
+
+                        nextRangeSeeds[nextRangeSeedCount++] = (range)
+                        {
+                            .start = transformEnd + 1,
+                            .end = currentRangeSeed.end
+                        };                   
+                        printf("      RIGHTY\r\n");
+                    }
+                    else
+                    {
+                        // ENCOMPASSING
+                        // TRANSFORM transform area, LEFT AND RIGHT UNTRANSFORMED (3 parts)
+                        nextRangeSeeds[nextRangeSeedCount++] = transform_range(transform.source, transformEnd, transform);
+
+                        nextRangeSeeds[nextRangeSeedCount++] = (range)
+                        {
+                            .start = currentRangeSeed.start,
+                            .end = transform.source - 1
+                        };                   
+
+                        nextRangeSeeds[nextRangeSeedCount++] = (range)
+                        {
+                            .start = transformEnd + 1,
+                            .end = currentRangeSeed.end
+                        };                   
+                        printf("      ENCOMPASSING\r\n");
+                    }
+
+#if 0
                     if (!(currentRangeSeed.end < transform.source || currentRangeSeed.start > transformEnd))
                     {
                         // some sort of collision, take the larger value of starts, take the lower value of ends
@@ -199,6 +329,11 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
                         };
                         printf("Always happens, man.. \r\n");
                     }
+                    else
+                    {
+                        
+                    }
+#endif
 
 #if 0
                     if (currentRangeSeed.start <= transform.source)
@@ -238,11 +373,13 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
 #endif
                 }
 
+#if 0
                 if (!foundTransform)
                 {
                     nextRangeSeeds[nextRangeSeedCount++] = currentRangeSeed;
                     printf("BOINKED -> %zu - %zu\r\n", currentRangeSeed.start, currentRangeSeed.end);
                 }
+#endif
             }
 
             memcpy(rangeSeeds, nextRangeSeeds, nextRangeSeedCount * sizeof(range));
@@ -257,8 +394,8 @@ transform_seeds(file_data file, seed_reading_mode seedReadingMode)
         
         for (u32 rangeSeedIndex = 0; rangeSeedIndex < rangeSeedCount; rangeSeedIndex++)
         {
-            printf("NEXT: %zu - %zu\r\n", nextRangeSeeds[rangeSeedIndex].start, nextRangeSeeds[rangeSeedIndex].end);
-            printf("CURR: %zu - %zu\r\n", rangeSeeds[rangeSeedIndex].start, rangeSeeds[rangeSeedIndex].end);
+            //printf("NEXT: %zu - %zu\r\n", nextRangeSeeds[rangeSeedIndex].start, nextRangeSeeds[rangeSeedIndex].end);
+            //printf("CURR: %zu - %zu\r\n", rangeSeeds[rangeSeedIndex].start, rangeSeeds[rangeSeedIndex].end);
 
             if (nextRangeSeeds[rangeSeedIndex].start < result)
                 result = nextRangeSeeds[rangeSeedIndex].start;
@@ -283,7 +420,7 @@ part_1()
 internal u64
 part_2()
 {
-    file_data file = read_to_end_of_file("input\\sample-input.txt");
+    file_data file = read_to_end_of_file("input\\day05-sample.txt");
     assert(file.size > 0);
     
     return 0; // transform_seeds(file, SEED_READING_MODE_ranges);
@@ -304,8 +441,6 @@ main(s32 argumentCount, char *arguments[])
 
     clock_t endTime = clock();
     u64 endCycles = __rdtsc();
-
-    assert(resultPart1 < 704533868 && "ERROR: TOO HIGH VALUE");
 
     debug_log("- Day 05 -\n");
     debug_log("Result Part 1: %lld (%d ms, %lld cycles passed)\n", resultPart1, (part1Time - startTime) * 1000 / CLOCKS_PER_SEC, (part1Cycles - startCycles));
